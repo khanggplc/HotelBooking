0c5d9cccb0b5fdb8d968eaa7b664d904bc5aa1ff
Hoang Van Khang <khanggplc@gmail.com>
2026-03-15 22:08:08 +0700

    .

---
+++ HotelBooking.Infrastructure/Persistence/DbInitializer.cs
@@ -0,0 +1,10 @@
+﻿using HotelBooking.Domain.Entities;
+using HotelBooking.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBooking.Infrastructure.Persistence;
+public static class DbInitializer
+{
+
+}
+
